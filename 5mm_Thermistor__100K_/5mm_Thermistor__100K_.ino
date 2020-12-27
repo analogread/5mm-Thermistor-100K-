20266c66d5abe436226488ae9ac46e285f820314
Analogread <75635913+analogread@users.noreply.github.com>
2020-12-27 20:12:34 +0700

    Add files via upload

---
+++ 5mm_Thermistor__100K_/5mm_Thermistor__100K_.ino
@@ -0,0 +1,36 @@
+#define LED 3
+int ThermistorPin = 0;
+int Vo;
+float R1 = 10000;
+float logR2, R2, T, Tc, Tf;
+float c1 = 1.009249522e-03, c2 = 2.378405444e-04, c3 = 2.019202697e-07;
+
+void setup() {
+  Serial.begin(9600);
+  pinMode(LED, OUTPUT);
+}
+
+void loop() {
+
+  Vo = analogRead(ThermistorPin);
+  R2 = R1 * (1023.0 / (float)Vo - 1.0);
+  logR2 = log(R2);
+  T = (1.0 / (c1 + c2 * logR2 + c3 * logR2 * logR2 * logR2));
+  Tc = T - 273.15;
+  Tf = (Tc * 9.0) / 5.0 + 32.0;
+
+  Serial.print("Temperature: ");
+  Serial.print(Tf);
+  Serial.print(" F; ");
+  Serial.print(Tc);
+  Serial.print(" C");
+  if (Tc >= 35) {
+    digitalWrite(LED, HIGH);
+    Serial.print("  :  เปิดไฟ\n");
+  } else {
+    digitalWrite(LED, LOW);
+    Serial.print("  :  ปิดไฟ\n");
+  }
+
+  delay(500);
+}
